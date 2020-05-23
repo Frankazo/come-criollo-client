c7b0debb12985d90ccab1c2511057b965e8effd7
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-05-23 18:55:32 -0400

    Redesign

--- src/components/Restaurant/RestaurantIndex.js
+++ src/components/Restaurant/RestaurantIndex.js
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import Spinner from '../Spinner'
-import Card from 'react-bootstrap/Card'
+import { Container, Row, Col } from 'react-bootstrap'
 import { indexRestaurant } from '../../api/restaurant'
+import styled from 'styled-components'
+
+const Img = styled.img`
+  width: auto;
+  height: 300px;
+  border-radius: 5px;
+
+  &:hover{
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 8px rgba(0, 0, 0, 1);
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-property: box-shadow;
+    transition-property: box-shadow;
+  }
+`
 
 const RestaurantIndex = props => {
   const [restaurants, setRestaurants] = useState(null)
@@ -32,23 +51,23 @@ const RestaurantIndex = props => {
   } else {
     restJsx = restaurants.map(rest => {
       return (
-        <Link style={{ marginBottom: '10px', color: 'black' }} key={rest._id} to={`/restaurant/${rest._id}`}>
-          <Card style={{ margin: '40px', width: '27rem', height: '15rem' }}>
-            <Card.Img variant="top" src={rest.imageUrl} />
-            <Card.Body>
-              <Card.Title>{rest.restName}</Card.Title>
-            </Card.Body>
-          </Card>
+        <Link style={{ color: 'black' }} key={rest._id} to={`/restaurant/${rest._id}`}>
+          <Col md={4} key={rest._id}>
+            <Img alt="restaurant image" src={rest.imageUrl} />
+            <div style={{ marginTop: '10px', width: '300px' }}>
+              <p style={{ fontSize: '1.4rem' }} >{rest.restName}</p>
+            </div>
+          </Col>
         </Link>
       )
     })
   }
   return (
-    <div>
-      <div className="row row-cols-1 row-cols-md-4">
+    <Container style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '40px' }} fluid="md">
+      <Row>
         {restJsx}
-      </div>
-    </div>
+      </Row>
+    </Container>
   )
 }
 
